7c0f9194714c81808046e04460bc5637a4408bd3
tungakanui <tung.ngthanh99@gmail.com>
2023-09-12 11:29:42 +0700

    add CV

--- mailer.php
+++ mailer.php
@@ -1,6 +1,6 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type");
     header("Content-Type: application/json");
 
