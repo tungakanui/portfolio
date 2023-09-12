66654addb48ea0615221eaa89923995cd3b190d7
tungakanui <tung.ngthanh99@gmail.com>
2023-09-12 13:57:07 +0700

    mailer

--- mailer.php
+++ mailer.php
@@ -1,9 +1,18 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type");
-    header("Content-Type: application/json");
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Credentials: true');    
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
+    }   
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 
+        exit(0);
+    } 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
